2679431cf6405e60de2623cc9d30e3d294daa54c
FuyuYoru <vvelichko.2000@mail.ru>
2025-10-27 23:50:45 +0500

    add sql install

---
+++ modules/test.currency/install/db/mysql/install.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS test_currency (
+    ID int(11) NOT NULL AUTO_INCREMENT,
+    CODE varchar(10) NOT NULL,
+    DATE datetime NOT NULL,
+    COURSE float NOT NULL,
+    PRIMARY KEY (ID)
+    );
